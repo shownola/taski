9eb1c984f613bd462844f95018dc598e9d707ced
SHERRY <swasieko@gmail.com>
2017-01-05 03:47:55 +0000

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def contact
+  end
+
+  def about
+    @title = "My Cool Page"
+  end
+
+  def home
+    @projects = Project.all
+  end
+  
+  def error
+  end
+  
+end
